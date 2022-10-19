b759436f933f5dbed49c0e759576a35cf3c525b9
warapon6430300862 <warapon.jitsook@gmail.com>
2022-10-19 19:50:05 +0700

    3 Page Successful

--- shortcut_devtool/src/components/Header.jsx
+++ shortcut_devtool/src/components/Header.jsx
@@ -1,57 +1,57 @@
-import React, { useState, useEffect } from 'react'
-import '../css/Header.css'
+import React, { useState } from "react";
+import ToolsGrid from "./ToolsGrid";
+import ToolsGrid2 from "./ToolsGrid2";
+import ToolsGrid3 from "./ToolsGrid3";
+import { Route, Link, Routes } from "react-router-dom";
+import "../css/Header.css";
+import "../css/App.css";
 
 const Header = () => {
-    const [search, setSearch] = useState("");
-    const data_grid = [
-        "w3school",
-        "programiz",
-        "mozilla developer",
-        "react docs",
-        "codepen",
-        "replit ide online",
-        "css gradient generator",
-        "css gradient animator",
-    ];
+  const [search, setSearch] = useState("");
 
-    function handleSetSearch(event) {
-        setSearch(event.target.value.toUpperCase());
-    }
+  function handleSetSearch() {
+    let text_value = document.getElementById("text-search");
+    setSearch(text_value.value.toUpperCase());
+    // clear input value ,reset state
+    setTimeout(() => {
+      setSearch("");
+      text_value.value = "";
+    }, 1500);
+  }
 
-    function handleSearchData() {
-        if (data_grid.includes(search.toLowerCase())) {
-            if (search.toLowerCase() === data_grid[0]) {
-                alert(`${ search } at row 1 / col 1`);
-            }
-            if (search.toLowerCase() === data_grid[1]) {
-                alert(`${ search } at row 1 / col 2`);
-            }
-            if (search.toLowerCase() === data_grid[2]) {
-                alert(`${ search } at row 2 / col 1`);
-            }
-            if (search.toLowerCase() === data_grid[3]) {
-                alert(`${ search } at row 2 / col 2`);
-            }
-        } else {
-            alert(`${ search } have not found`);
-        }
-    }
-
-    return (
-      <div>
-        <header>
-            <div className="header-name">
-                <h1>ShortCut DevTools</h1>
-            </div>
-            <div className="input-search">
-                <label for="">
-                <input type="text" name="text-search" onChange={handleSetSearch}/>
-                <button type="submit" onClick={handleSearchData}>OK</button>
-                </label>
-            </div>
-        </header>
+  return (
+    <div>
+      <header>
+        <div className="header-name">
+          <h1>ShortCut DevTools</h1>
+        </div>
+        <div className="input-search">
+          <label for="">
+            <input type="text" id="text-search" />
+            <button type="submit" onClick={handleSetSearch}>
+              OK
+            </button>
+          </label>
+        </div>
+      </header>
+      <div className="page-number-container">
+        <span className="item-element">
+          <Link to="/">Website</Link>
+        </span>
+        <span className="item-element">
+          <Link to="/devtools">DevTools</Link>
+        </span>
+        <span className="item-element">
+          <Link to="/api">API</Link>
+        </span>
       </div>
-    );
-}
+      <Routes>
+        <Route path="/" element={<ToolsGrid value={search} />} />
+        <Route path="/devtools" element={<ToolsGrid2 value={search} />} />
+        <Route path="/api" element={<ToolsGrid3 value={search} />}></Route>
+      </Routes>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
--- shortcut_devtool/src/components/ToolsGrid2.jsx
+++ shortcut_devtool/src/components/ToolsGrid2.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import codepen_cover from '../image/image_devtools/Screenshot 2022-10-17 135156.png'
-import replit_cover from '../image/image_devtools/Screenshot 2022-10-17 135244.png'
-import css_gradient_cover from '../image/image_devtools/Screenshot 2022-10-17 135322.png'
-import css_gradient_animator_cover from '../image/image_devtools/Screenshot 2022-10-17 140551.png'
-import '../css/ToolsGrid.css'
+import React, { useEffect } from "react";
+import codepen_cover from "../image/image_devtools/Screenshot 2022-10-17 135156.png";
+import replit_cover from "../image/image_devtools/Screenshot 2022-10-17 135244.png";
+import css_gradient_cover from "../image/image_devtools/Screenshot 2022-10-17 135322.png";
+import css_gradient_animator_cover from "../image/image_devtools/Screenshot 2022-10-17 140551.png";
+import "../css/ToolsGrid.css";
 
-const ToolsGrid2 = () => {
+const ToolsGrid2 = ({ value }) => {
   const data_grid = [
     {
       img: codepen_cover,
@@ -28,21 +28,51 @@ const ToolsGrid2 = () => {
       url: "https://www.gradient-animator.com/",
     },
   ];
+
+  const search = value.toUpperCase();
+
+  function dynamicTitle() {
+    document.title = "Devtools - IDE";
+  }
+
+  function handleDynamicSearch() {
+    let item_container = document.querySelectorAll(".item-container");
+    let text_name = document.querySelectorAll("h2");
+    for (let i = 0; i < text_name.length; i++) {
+      if (text_name[i].innerHTML === search) {
+        setTimeout(() => {
+          text_name[i].style.backgroundColor = "black";
+          text_name[i].style.color = "white";
+          text_name[i].style.transition = "all 1s ease-in-out";
+          item_container[i].scrollIntoView();
+        }, 200);
+      } else {
+        setTimeout(() => {
+          text_name[i].style.backgroundColor = "white";
+          text_name[i].style.color = "black";
+          text_name[i].style.transition = "all 1s ease-in-out";
+        }, 200);
+      }
+    }
+  }
+
+  useEffect(() => {
+    handleDynamicSearch();
+  });
+
   return (
-    <div className='main'>
-      <div className='grid-container'>
-        {
-          data_grid.map(item => (
-            <div className='item-container'>
-              <img src={item.img} alt="" />
-              <h2>{item.name.toUpperCase()}</h2>
-              <a href={item.url}>Go To Page</a>
-            </div>
-          ))
-        }
+    <div className="main">
+      <div className="grid-container" onLoad={dynamicTitle}>
+        {data_grid.map((item) => (
+          <div className="item-container">
+            <img src={item.img} alt="" />
+            <h2>{item.name.toUpperCase()}</h2>
+            <a href={item.url}>Go To Page</a>
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ToolsGrid2
+export default ToolsGrid2;
